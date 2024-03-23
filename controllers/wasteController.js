40bfd43c50260765ebcf4ba5ab339ec5a7cf22d3
Madi Fuller <madi.fuller595@gmail.com>
2024-03-23 16:23:09 -0400

    get endpoint for waste items list

--- controllers/wasteController.js
+++ controllers/wasteController.js
@@ -1,5 +1,14 @@
-// const addWasteItem = async (req, res) => {
-//     try {
-//         const data = await
-//     }
-// }
+const knex = require("knex")(require("../knexfile.js"));
+
+const wasteItems = async(_req, res) => {
+    try {
+        const data = await knex('waste_items')
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(400).send(`Error retrieving waste item list: ${error}`);
+    }
+}
+
+module.exports = {
+    wasteItems
+};
--- routes/waste.js
+++ routes/waste.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const wasteController = require('../controllers/wasteController');
 
-router.post('/', (req, res) => {
-    const wasteItem = req.body;
-    res.status(201).json({message: "Waste item added successfully"});
-});
+// router.post('/', (req, res) => {
+//     const wasteItem = req.body;
+//     res.status(201).json({message: "Waste item added successfully"});
+// });
+
+router
+.route('/')
+.get(wasteController.wasteItems);
 
 module.exports = router;
