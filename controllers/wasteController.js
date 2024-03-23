1e33fb59c7367d5499c56019878cdbbcb688d8e2
Madi Fuller <madi.fuller595@gmail.com>
2024-03-23 17:15:16 -0400

    add delete route for waste items

--- controllers/wasteController.js
+++ controllers/wasteController.js
@@ -1,76 +1,81 @@
-const { up } = require("../migrations/20240323190947_create_user_waste_items_tables.js");
+const {
+  up,
+} = require("../migrations/20240323190947_create_user_waste_items_tables.js");
 
 const knex = require("knex")(require("../knexfile.js"));
 
-const wasteItems = async(_req, res) => {
-    try {
-        const data = await knex('waste_items')
-        res.status(200).json(data);
-    } catch (error) {
-        res.status(400).send(`Error retrieving waste item list: ${error}`);
-    }
-}
+const wasteItems = async (_req, res) => {
+  try {
+    const data = await knex("waste_items");
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(400).send(`Error retrieving waste item list: ${error}`);
+  }
+};
 
 const addWasteItem = async (req, res) => {
-    try {
-        const { name, category, quantity } = req.body;
+  try {
+    const { name, category, quantity } = req.body;
 
-        if (!name || !category || !quantity) {
-            return res.status(400).json({ message: "Missing required fields"});
-        }
-        const newWasteItem = {name, category, quantity};
-        const result = await knex("waste_items").insert(newWasteItem);
-        const newWasteItemId = result[0];
-        const createdWasteItem = await knex("waste_items").where( { id: newWasteItemId});
-
-        res.status(201).json(createdWasteItem);
-    } catch(error) {
-        res.status(500).json({message: `Unable to add new waste item: ${error}`});
+    if (!name || !category || !quantity) {
+      return res.status(400).json({ message: "Missing required fields" });
     }
-}
+    const newWasteItem = { name, category, quantity };
+    const result = await knex("waste_items").insert(newWasteItem);
+    const newWasteItemId = result[0];
+    const createdWasteItem = await knex("waste_items").where({
+      id: newWasteItemId,
+    });
+
+    res.status(201).json(createdWasteItem);
+  } catch (error) {
+    res.status(500).json({ message: `Unable to add new waste item: ${error}` });
+  }
+};
 
 const editWasteItem = async (req, res) => {
-    try {
-        const rowsUpdated = await knex("waste_items")
-        .where({id: req.params.id})
-        .update(req.body);
+  try {
+    const rowsUpdated = await knex("waste_items")
+      .where({ id: req.params.id })
+      .update(req.body);
 
-        if (rowsUpdated === 0) {
-            return res.status(404).json({
-                message: `Waste item with ID ${req.params.id} not found`
-            });
-        }
-        const updatedWasteItem = await knex("waste_items").where({
-            id: req.params.id
-        });
-
-        res.json(updatedWasteItem[0]);
-    } catch (error) {
-        res.status(500).json({
-            message: `Unable to edit data for item with ID ${req.params.id}`
-        });
+    if (rowsUpdated === 0) {
+      return res.status(404).json({
+        message: `Waste item with ID ${req.params.id} not found`,
+      });
     }
+    const updatedWasteItem = await knex("waste_items").where({
+      id: req.params.id,
+    });
+
+    res.json(updatedWasteItem[0]);
+  } catch (error) {
+    res.status(500).json({
+      message: `Unable to edit data for item with ID ${req.params.id}`,
+    });
+  }
 };
 
 const deleteWasteItem = async (req, res) => {
-    try {
-        const rowDeleted = await knex("waste_items")
-        .where({id: req.params.id})
-        .del();
+  try {
+    const rowDeleted = await knex("waste_items")
+      .where({ id: req.params.id })
+      .del();
 
-        if(rowDeleted === 0) {
-            return res.status(500).json({ message: `Item with id: ${req.params.id} not found`});
-        }
-        return res.sendStatus(204);
-    } catch (error) {
-        res.status(500).json({message: `Unable to delete warehouse: ${error}`
-        });
+    if (rowDeleted === 0) {
+      return res
+        .status(500)
+        .json({ message: `Item with id: ${req.params.id} not found` });
     }
-}
+    return res.sendStatus(204);
+  } catch (error) {
+    res.status(500).json({ message: `Unable to delete warehouse: ${error}` });
+  }
+};
 
 module.exports = {
-    wasteItems,
-    addWasteItem,
-    editWasteItem,
-    deleteWasteItem
+  wasteItems,
+  addWasteItem,
+  editWasteItem,
+  deleteWasteItem,
 };
--- routes/waste.js
+++ routes/waste.js
@@ -1,14 +1,15 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const wasteController = require('../controllers/wasteController');
+const wasteController = require("../controllers/wasteController");
 
 router
-.route('/')
-.get(wasteController.wasteItems)
-.post(wasteController.addWasteItem);
+  .route("/")
+  .get(wasteController.wasteItems)
+  .post(wasteController.addWasteItem);
 
 router
-.route('/:id')
-.put(wasteController.editWasteItem);
+  .route("/:id")
+  .put(wasteController.editWasteItem)
+  .delete(wasteController.deleteWasteItem);
 
 module.exports = router;
