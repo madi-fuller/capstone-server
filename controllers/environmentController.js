0a472e17ff55ba3c385f2ec134b552dbd588557f
Madi Fuller <madi.fuller595@gmail.com>
2024-03-26 13:29:27 -0400

    get environmental information by ID endpoint

--- controllers/environmentController.js
+++ controllers/environmentController.js
@@ -1,14 +1,24 @@
 const knex = require("knex")(require("../knexfile.js"));
 
-const environmentImpact = async (_req, res) => {
-    try {
-        const data = await knex ("environmental_impacts");
-        res.status(200).json(data);
-    }catch (error) {
-        res.status(400).send(`Error retrieving environmental impact information ${error}`);
+const environmentImpact = async (req, res) => {
+  try {
+    const data = await knex("environmental_impacts").where({
+      id: req.params.id,
+    });
+
+    if (data.length === 0) {
+        return res.status(404).json({
+            message: `Item with ID ${req.params.id} was not found`
+        });
     }
+    res.status(200).json(data);
+  } catch (error) {
+    res
+      .status(500)
+      .send(`Error retrieving environmental impact information ${error}`);
+  }
 };
 
 module.exports = {
-    environmentImpact
+  environmentImpact,
 };
--- routes/environment.js
+++ routes/environment.js
@@ -2,8 +2,12 @@ const express = require("express");
 const router = express.Router();
 const environmentController = require("../controllers/environmentController");
 
+// router
+//     .route("/")
+//     .get(environmentController.environmentImpact);
+
 router
-    .route("/")
+    .route("/:id")
     .get(environmentController.environmentImpact);
 
 module.exports = router;
