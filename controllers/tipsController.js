e249b1b156d553a1133a9f6290c95f0d5398a2ba
Madi Fuller <madi.fuller595@gmail.com>
2024-03-31 22:33:29 -0400

    change endpoint to get 3 random tips

--- controllers/tipsController.js
+++ controllers/tipsController.js
@@ -2,10 +2,10 @@ const knex = require("knex")(require("../knexfile.js"));
 
 const wasteTips = async (_req, res) => {
     try {
-       const data = await knex("food_waste_tips");
-       res.status(200).json(data);
+        const data = await knex("food_waste_tips").orderByRaw("RAND()").limit(3);
+        res.status(200).json(data);
     } catch (error) {
-        res.status(400).send(`Error retrieving waste tips:, ${error}`);
+        res.status(400).send(`Error retrieving random waste tips: ${error}`);
     }
 };
 
